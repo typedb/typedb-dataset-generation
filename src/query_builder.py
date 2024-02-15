c192c0c90bd3de1fd3668d85ba242442b7604f3a
James Whiteside <james.whiteside@vaticle.com>
2024-02-15 11:54:03 +0000

    Minor bug fixes.

--- src/query_builder.py
+++ src/query_builder.py
@@ -148,35 +148,35 @@ class QueryBuilder:
 
     def country(self, name: str) -> str:
         query = " ".join((
-            f"insert",
-            f"$country isa country;",
-            f"$country has name '{name}';",
+            f"""insert""",
+            f"""$country isa country;""",
+            f"""$country has name "{name}";""",
         ))
 
         return query
 
     def state(self, name: str, country_name: str) -> str:
         query = " ".join((
-            f"match",
-            f"$country isa country;",
-            f"$country has name '{country_name}';",
-            f"insert",
-            f"$state isa state;",
-            f"$state has name '{name}';",
-            f"(location: $country, located: $state) isa locating;",
+            f"""match""",
+            f"""$country isa country;""",
+            f"""$country has name "{country_name}";""",
+            f"""insert""",
+            f"""$state isa state;""",
+            f"""$state has name "{name}";""",
+            f"""(location: $country, located: $state) isa locating;""",
         ))
 
         return query
 
     def city(self, name: str, parent_type: ParentPlaceType, parent_name: str) -> str:
         query = " ".join((
-            f"match",
-            f"${parent_type.value} isa {parent_type.value};",
-            f"${parent_type.value} has name '{parent_name}';",
-            f"insert",
-            f"$city isa city;",
-            f"$city has name '{name}';",
-            f"(location: ${parent_type.value}, located: $city) isa locating;",
+            f"""match""",
+            f"""${parent_type.value} isa {parent_type.value};""",
+            f"""${parent_type.value} has name "{parent_name}";""",
+            f"""insert""",
+            f"""$city isa city;""",
+            f"""$city has name "{name}";""",
+            f"""(location: ${parent_type.value}, located: $city) isa locating;""",
         ))
 
         return query
@@ -201,63 +201,63 @@ class QueryBuilder:
             stock = self._random.randint(0, 20)
 
         query = " ".join((
-            f"insert",
-            f"$book isa {book_type.value};",
-            f"$book has isbn-13 '{isbn_13}';",
-            f"$book has title '{title}';",
-            f"$book has page-count {page_count};",
-            f"$book has price {price};",
-            f"$book has stock {stock};",
+            f"""insert""",
+            f"""$book isa {book_type.value};""",
+            f"""$book has isbn-13 "{isbn_13}";""",
+            f"""$book has title "{title}";""",
+            f"""$book has page-count {page_count};""",
+            f"""$book has price {price};""",
+            f"""$book has stock {stock};""",
         ))
 
         if isbn_10 is not None:
-            query += f"$book has isbn-10 '{isbn_10}';"
+            query += f""" $book has isbn-10 "{isbn_10}";"""
 
         for contributor in contributors:
             contributor_name = contributor[0]
             contributor_role = contributor[1]
 
             query += " " + " ".join((
-                f"match",
-                f"$contributor-type type contributor;",
-                f"not {{",
-                f"$contributor isa $contributor-type;",
-                f"$contributor has name '{contributor_name}';",
-                f"}};",
-                f"insert",
-                f"$contributor isa $contributor-type;",
-                f"$contributor has name '{contributor_name}';",
-                f"match",
-                f"$book isa {book_type.value};",
-                f"$book has isbn-13 '{isbn_13}';",
-                f"$contributor isa contributor;",
-                f"$contributor has name '{contributor_name}';",
-                f"insert",
-                f"(work: $book, {contributor_role.value}: $contributor) isa {contributor_role.relation_type()};",
+                f"""match""",
+                f"""$contributor-type type contributor;""",
+                f"""not {{""",
+                f"""$contributor isa $contributor-type;""",
+                f"""$contributor has name "{contributor_name}";""",
+                f"""}};""",
+                f"""insert""",
+                f"""$contributor isa $contributor-type;""",
+                f"""$contributor has name "{contributor_name}";""",
+                f"""match""",
+                f"""$book isa {book_type.value};""",
+                f"""$book has isbn-13 "{isbn_13}";""",
+                f"""$contributor isa contributor;""",
+                f"""$contributor has name "{contributor_name}";""",
+                f"""insert""",
+                f"""(work: $book, {contributor_role.value}: $contributor) isa {contributor_role.relation_type()};""",
             ))
 
         query += " " + " ".join((
-            f"match",
-            f"$publisher-type type publisher;",
-            f"not {{",
-            f"$publisher isa $publisher-type;",
-            f"$publisher has name '{publisher_name}';",
-            f"}};",
-            f"insert",
-            f"$publisher isa $publisher-type;",
-            f"$publisher has name '{publisher_name}';",
-            f"match",
-            f"$book isa {book_type.value};",
-            f"$book has isbn-13 '{isbn_13}';",
-            f"$publisher isa publisher;",
-            f"$publisher has name '{publisher_name}';",
-            f"$city isa city;",
-            f"$city has name '{publication_city}';",
-            f"insert",
-            f"$publication isa publication;",
-            f"$publication has year {publication_year};",
-            f"(published: $book, publisher: $publisher, publication: $publication) isa publishing;",
-            f"(location: $city, located: $publication) isa locating;",
+            f"""match""",
+            f"""$publisher-type type publisher;""",
+            f"""not {{""",
+            f"""$publisher isa $publisher-type;""",
+            f"""$publisher has name "{publisher_name}";""",
+            f"""}};""",
+            f"""insert""",
+            f"""$publisher isa $publisher-type;""",
+            f"""$publisher has name "{publisher_name}";""",
+            f"""match""",
+            f"""$book isa {book_type.value};""",
+            f"""$book has isbn-13 "{isbn_13}";""",
+            f"""$publisher isa publisher;""",
+            f"""$publisher has name "{publisher_name}";""",
+            f"""$city isa city;""",
+            f"""$city has name "{publication_city}";""",
+            f"""insert""",
+            f"""$publication isa publication;""",
+            f"""$publication has year {publication_year};""",
+            f"""(published: $book, publisher: $publisher, publication: $publication) isa publishing;""",
+            f"""(location: $city, located: $publication) isa locating;""",
         ))
 
         return query
@@ -347,23 +347,23 @@ class QueryBuilder:
         promotion_id = self._get_new_promotion_id()
 
         query = " ".join((
-            f"insert",
-            f"$promotion isa promotion;",
-            f"$promotion has id '{promotion_id}';",
-            f"$promotion has start-timestamp {start_timestamp};",
-            f"$promotion has end-timestamp {end_timestamp};",
-            f"$promotion has discount {discount};",
+            f"""insert""",
+            f"""$promotion isa promotion;""",
+            f"""$promotion has id "{promotion_id}";""",
+            f"""$promotion has start-timestamp {start_timestamp};""",
+            f"""$promotion has end-timestamp {end_timestamp};""",
+            f"""$promotion has discount {discount};""",
         ))
 
         for isbn_13 in book_isbn_13s:
             query += " " + " ".join((
-                f"match",
-                f"$book isa book;",
-                f"$book has isbn-13 '{isbn_13}';",
-                f"$promotion isa promotion;",
-                f"$promotion has id '{promotion_id}';",
-                f"insert",
-                f"(promotion: $promotion, included: $book) isa promotion-inclusion;",
+                f"""match""",
+                f"""$book isa book;""",
+                f"""$book has isbn-13 "{isbn_13}";""",
+                f"""$promotion isa promotion;""",
+                f"""$promotion has id "{promotion_id}";""",
+                f"""insert""",
+                f"""(promotion: $promotion, included: $book) isa promotion-inclusion;""",
             ))
 
         return query
@@ -375,15 +375,15 @@ class QueryBuilder:
         user_id = self._get_new_user_id()
 
         query = " ".join((
-            f"match",
-            f"$city isa city;",
-            f"$city has name '{city_name}';",
-            f"insert",
-            f"$user isa user;",
-            f"$user has id '{user_id}';",
-            f"$user has name '{name}';",
-            f"$user has birth-date {birth_date};",
-            f"(location: $city, located: $user) isa locating;",
+            f"""match""",
+            f"""$city isa city;""",
+            f"""$city has name "{city_name}";""",
+            f"""insert""",
+            f"""$user isa user;""",
+            f"""$user has id "{user_id}";""",
+            f"""$user has name "{name}";""",
+            f"""$user has birth-date {birth_date};""",
+            f"""(location: $city, located: $user) isa locating;""",
         ))
 
         return query
@@ -400,7 +400,7 @@ class QueryBuilder:
     ) -> str:
         order_id = self._get_new_order_id()
 
-        if type(order_lines) is list[int]:
+        if type(order_lines[0]) is int:
             book_quantities = dict()
 
             for quantity in order_lines:
@@ -426,44 +426,44 @@ class QueryBuilder:
             user_id = self._get_random_user_id()
 
         query = " ".join((
-            f"match",
-            f"$courier-type type courier;",
-            f"not {{",
-            f"$courier isa $courier-type;",
-            f"$courier has name '{courier_name}';",
-            f"}};",
-            f"insert",
-            f"$courier isa $courier-type;",
-            f"$courier has name '{courier_name}';",
-            f"match",
-            f"$city isa city;",
-            f"$city has name '{city_name}';",
-            f"not {{",
-            f"$address isa address;",
-            f"$address has street '{address_street}';",
-            f"(location: $city, located: $address) isa locating;",
-            f"}};",
-            f"insert",
-            f"$address isa address;",
-            f"$address has street '{address_street}';",
-            f"(location: $city, located: $address) isa locating;",
-            f"match",
-            f"$user isa user;",
-            f"$user has id '{user_id}';",
-            f"$courier isa courier;",
-            f"$courier has name '{courier_name}';",
-            f"$city isa city;",
-            f"$city has name '{city_name}';",
-            f"$address isa address;",
-            f"$address has street '{address_street}';",
-            f"(location: $city, located: $address) isa locating;",
-            f"insert",
-            f"$order isa order;",
-            f"$order has order-id '{order_id}';",
-            f"$order has status '{status.value}';",
-            f"$execution (action: $order, executor: $user) isa action-execution;",
-            f"$execution has timestamp {execution_timestamp};",
-            f"(delivered: $order, deliverer: $courier, destination: $address) isa delivery;",
+            f"""match""",
+            f"""$courier-type type courier;""",
+            f"""not {{""",
+            f"""$courier isa $courier-type;""",
+            f"""$courier has name "{courier_name}";""",
+            f"""}};""",
+            f"""insert""",
+            f"""$courier isa $courier-type;""",
+            f"""$courier has name "{courier_name}";""",
+            f"""match""",
+            f"""$city isa city;""",
+            f"""$city has name "{city_name}";""",
+            f"""not {{""",
+            f"""$address isa address;""",
+            f"""$address has street "{address_street}";""",
+            f"""(location: $city, located: $address) isa locating;""",
+            f"""}};""",
+            f"""insert""",
+            f"""$address isa address;""",
+            f"""$address has street "{address_street}";""",
+            f"""(location: $city, located: $address) isa locating;""",
+            f"""match""",
+            f"""$user isa user;""",
+            f"""$user has id "{user_id}";""",
+            f"""$courier isa courier;""",
+            f"""$courier has name "{courier_name}";""",
+            f"""$city isa city;""",
+            f"""$city has name "{city_name}";""",
+            f"""$address isa address;""",
+            f"""$address has street "{address_street}";""",
+            f"""(location: $city, located: $address) isa locating;""",
+            f"""insert""",
+            f"""$order isa order;""",
+            f"""$order has id "{order_id}";""",
+            f"""$order has status "{status.value}";""",
+            f"""$execution (action: $order, executor: $user) isa action-execution;""",
+            f"""$execution has timestamp {execution_timestamp};""",
+            f"""(delivered: $order, deliverer: $courier, destination: $address) isa delivery;""",
         ))
 
         for line in order_lines:
@@ -471,14 +471,14 @@ class QueryBuilder:
             line_quantity = line[1]
 
             query += " " + " ".join((
-                f"match",
-                f"$order isa order;",
-                f"$order has id '{order_id}';",
-                f"$book isa book;",
-                f"$book has isbn-13 '{book_isbn_13}';",
-                f"insert",
-                f"$line (order: $order, item: $book) isa order-line;",
-                f"$line has quantity {line_quantity};",
+                f"""match""",
+                f"""$order isa order;""",
+                f"""$order has id "{order_id}";""",
+                f"""$book isa book;""",
+                f"""$book has isbn-13 "{book_isbn_13}";""",
+                f"""insert""",
+                f"""$line (order: $order, item: $book) isa order-line;""",
+                f"""$line has quantity {line_quantity};""",
             ))
 
         return query
@@ -494,17 +494,17 @@ class QueryBuilder:
             user_id = self._get_random_user_id()
 
         query = " ".join((
-            f"match",
-            f"$book isa book;",
-            f"$book has isbn-13 '{book_isbn_13}';",
-            f"$user isa user;",
-            f"$user has id '{user_id}';",
-            f"insert",
-            f"$review isa review;",
-            f"$review has score {score};",
-            f"(review: $review, rated: $book) isa rating;",
-            f"$execution (action: $review, executor: $user) isa action-execution;",
-            f"$execution has timestamp {execution_timestamp};",
+            f"""match""",
+            f"""$book isa book;""",
+            f"""$book has isbn-13 "{book_isbn_13}";""",
+            f"""$user isa user;""",
+            f"""$user has id "{user_id}";""",
+            f"""insert""",
+            f"""$review isa review;""",
+            f"""$review has score {score};""",
+            f"""(review: $review, rated: $book) isa rating;""",
+            f"""$execution (action: $review, executor: $user) isa action-execution;""",
+            f"""$execution has timestamp {execution_timestamp};""",
         ))
 
         return query
@@ -520,14 +520,14 @@ class QueryBuilder:
             user_id = self._get_random_user_id()
 
         query = " ".join((
-            f"match",
-            f"$user isa user;",
-            f"$user has id '{user_id}';",
-            f"insert",
-            f"$login isa login;",
-            f"$login has success {str(success).lower()};",
-            f"$execution (action: $login, executor: $user) isa action-execution;",
-            f"$execution has timestamp {execution_timestamp};",
+            f"""match""",
+            f"""$user isa user;""",
+            f"""$user has id "{user_id}";""",
+            f"""insert""",
+            f"""$login isa login;""",
+            f"""$login has success {str(success).lower()};""",
+            f"""$execution (action: $login, executor: $user) isa action-execution;""",
+            f"""$execution has timestamp {execution_timestamp};""",
         ))
 
         return query
